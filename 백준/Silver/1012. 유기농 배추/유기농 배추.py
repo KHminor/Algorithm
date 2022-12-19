b3ea9cce3c9164d1f3cfd7778af2672beef96323
KHminor <109326297+KHminor@users.noreply.github.com>
2022-12-19 13:55:01 +0900

    [Silver II] Title: 유기농 배추, Time: 220 ms, Memory: 39780 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,6 +1,6 @@
 import sys
-from pprint import pprint
 from collections import deque
+from pprint import pprint
 # 테스트케이스
 t = int(sys.stdin.readline())
 
@@ -9,13 +9,13 @@ for _ in range(t):
     m,n,k = map(int,sys.stdin.readline().split())
 
     # 재배장
-    arr = [[0]*m for _ in range(n)]
-    visited = [[0]*m for _ in range(n)]
+    arr = [[0]*n for _ in range(m)]
+    visited = [[0]*n for _ in range(m)]
 
     # 배추 심기
     for _ in range(k):
         i,j = map(int,sys.stdin.readline().split())
-        arr[j][i] = 1
+        arr[i][j] = 1
 
     # 배추흰지렁이 필요한 개수 찾기 (델타)
     cnt = 0
@@ -23,25 +23,24 @@ for _ in range(t):
     di = [-1,1,0,0]
     dj = [0,0,-1,1]
 
-    for i in range(n):
-        for j in range(m):
+    for a in range(m):
+        for s in range(n):
             # 배추가 심어져있고, 방문하지 않은 곳일 경우
-            if arr[i][j] == 1 and not visited[i][j]:
+            if arr[a][s] == 1 and not visited[a][s]:
                 # BFS
-                q = deque([(i,j)])
+                q = deque([(a,s)])
                 while q:
                     # q 사용
-                    a,s = q.popleft()
+                    i,j = q.popleft()
                     # 해당 좌표가 방문한 곳일 경우 다시 q에서 뽑아 조사
-                    if visited[a][s]:
+                    if visited[i][j]:
                         continue
-                    visited[a][s] = 1
+                    visited[i][j] = 1
                     # 델타 탐색
                     for x in range(4):
                         # 재배장 범위 안에 있어야하고, 이동 위치에 배추가 있어야하고, 해당 위치가 방문하지 않은 곳일 경우
-                        if 0 <= a+di[x] < n and 0 <= s+dj[x] < m and arr[a+di[x]][s+dj[x]] and not visited[a+di[x]][s+dj[x]]:
-                            q.append((a+di[x],s+dj[x]))
-
+                        if 0 <= i+di[x] < m and 0 <= j+dj[x] < n and arr[i+di[x]][j+dj[x]] and not visited[i+di[x]][j+dj[x]]:
+                            q.append((i+di[x],j+dj[x]))
                 cnt += 1
 
     print(cnt)
