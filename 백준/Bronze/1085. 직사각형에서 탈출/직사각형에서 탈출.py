6b0666a8a7726c18e4e835edbf498cb2c9fc59b5
KHminor <109326297+KHminor@users.noreply.github.com>
2023-02-26 17:16:58 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,17 +1,2 @@
-import sys
-x,y,w,h = map(int,sys.stdin.readline().split())
-
-si = sj = result = 0
-
-li = []
-
-li.append(x-si)
-li.append(y-sj)
-li.append(w-x)
-li.append(h-y)
-
-for i in li:
-    if i < 0:
-        i *= -1
-
-print(min(li))
+x,y,w,h = map(int,input().split())
+print(min(x-0,w-x,y-0,h-y))
