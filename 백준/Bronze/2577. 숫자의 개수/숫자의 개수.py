99cb2c7934ed253e58fd5d37a0c95c3936343857
KHminor <109326297+KHminor@users.noreply.github.com>
2023-04-26 22:13:13 +0900

    [Bronze II] Title: 숫자의 개수, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,12 +1,7 @@
-a = int(input())
-b = int(input())
-c = int(input())
-
-gob = list(str(a*b*c))
-ch_li = [str(i) for i in range(10)]
-resutl = [0]*10
-for i in range(len(ch_li)):
-    for j in gob:
-        if ch_li[i] == j:
-            resutl[i] += 1
-    print(resutl[i])
+import math
+li = math.prod([int(input()) for _ in range(3)])
+dic = {i:0 for i in range(10)}
+for i in str(li):
+    dic[int(i)] += 1
+for i in range(10):
+    print(dic[i])
