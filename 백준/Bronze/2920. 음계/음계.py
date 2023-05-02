33b952d6f8127974fda56ac5f3db8155dd19e221
KHminor <109326297+KHminor@users.noreply.github.com>
2023-05-02 22:43:24 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,16 +1,4 @@
 li = list(map(int,input().split()))
-ad = ds = result = 0
-for i in range(1,len(li)):
-    if li[i-1]+1 == li[i] :
-        ad += 1
-    elif li[i-1]-1 == li[i]:
-        ds += 1
-    if ad > 0 and ds > 0:
-        break
-if len(li)-1 == ad:
-    result = 'ascending'
-elif len(li)-1 == ds:
-    result = 'descending'
-else:
-    result = 'mixed'
-print(result)
+if li == [1,2,3,4,5,6,7,8]: print('ascending')
+elif li == [8,7,6,5,4,3,2,1]: print('descending')
+else: print('mixed')
