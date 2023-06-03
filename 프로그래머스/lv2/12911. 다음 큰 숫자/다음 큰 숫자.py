0d4f19e40b96db9d18c146bb85ebf48381507ede
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-03 11:07:54 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,5 +1,4 @@
 def solution(n):
-    n_bin_cnt = bin(n).replace('0', '').lstrip('0b').count('1')
+    n_bin_cnt = bin(n).count('1')
     for i in range(n+1, 1000001):
-        if n_bin_cnt == bin(i).replace('0', '').lstrip('0b').count('1'): 
-            return i
+        if n_bin_cnt == bin(i).count('1'): return i
