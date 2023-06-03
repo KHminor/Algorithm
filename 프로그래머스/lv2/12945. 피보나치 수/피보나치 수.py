686b2fc3146563ea4e190b59dc974b458bd21f66
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-03 12:16:45 +0900

    [level 2] Title: 피보나치 수, Time: 27.97 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,10 +1,15 @@
-import sys
-sys.setrecursionlimit(200000)
-li = [0,1] + [ -1 for _ in range(100000)]
+# import sys
+# sys.setrecursionlimit(200000)
+# li = [0,1] + [ -1 for _ in range(100000)]
+
+# def solution(n):
+#     if li[n] != -1 : return li[n]
+#     li[n] = ((solution(n-1) + solution(n-2)))%1234567
+#     return li[n]%1234567
 
 def solution(n):
-    if li[n] != -1 : return li[n]
-    li[n] = ((solution(n-1) + solution(n-2)))%1234567
-    return li[n]%1234567
-
+    f_list = [0,1]
+    for i in range(2,n+1):
+        f_list.append((f_list[i-2]%1234567+f_list[i-1]%1234567)%1234567)
+    return f_list[-1]
 
