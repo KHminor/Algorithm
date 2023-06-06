f239a66f5dbbd3cb7414a648adaf290f81027617
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-06 11:51:33 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,9 +1,9 @@
 def solution(brown, yellow):
-    size = brown+yellow
-    for i in range(size+1,1,-1):
-        if size%i==0:
-            div = size/i
-            if (i-2) * (div - 2) == yellow:
-                return [i,div]
+    c_size = brown + yellow
+    for i in range(int(c_size**(1/2)),1,-1):
+        if c_size%i == 0:
+            side = c_size/i
+            if (i-2)*(side-2) == yellow:
+                return [max([i,side]), min([i,side])]
             
         
