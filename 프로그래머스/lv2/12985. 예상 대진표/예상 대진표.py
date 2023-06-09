7d6d7199eaff8866d7181c28d2d9d144991dfb15
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-09 11:35:30 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,15 +1,7 @@
 def solution(n,a,b):
-    li = [i+1 for i in range(n)]
-    cnt, flag = 0, False
-    while True:
-        cnt += 1
-        new_li = []
-        for i in range(len(li)//2):
-            if li[i*2] in [a,b] and li[i*2+1] in [a,b]:
-                flag = True
-                break
-            elif li[i*2] in [a,b]: new_li.append(li[i*2])
-            else : new_li.append(li[i*2+1])
-        li = new_li
-        if flag: break
-    return cnt
+    answer = 0
+    while a != b:
+        answer += 1
+        a, b = (a+1)//2, (b+1)//2
+
+    return answer
