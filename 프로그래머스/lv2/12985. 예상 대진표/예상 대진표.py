53eaadcc41aac8bb13c7bda16523f0a37e76ae2a
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-09 11:33:45 +0900

    [level 2] Title: 예상 대진표, Time: 425.02 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,15 @@
+def solution(n,a,b):
+    li = [i+1 for i in range(n)]
+    cnt, flag = 0, False
+    while True:
+        cnt += 1
+        new_li = []
+        for i in range(len(li)//2):
+            if li[i*2] in [a,b] and li[i*2+1] in [a,b]:
+                flag = True
+                break
+            elif li[i*2] in [a,b]: new_li.append(li[i*2])
+            else : new_li.append(li[i*2+1])
+        li = new_li
+        if flag: break
+    return cnt
