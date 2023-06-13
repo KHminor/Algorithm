9f459d920e982b09c514da4ec0b645ae09b7885a
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-13 15:42:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,6 +1,5 @@
 def solution(n):
     x,y = 1, 2
     if n in [1,2]: return n % 1234567
-    for i in range(n-2): 
-        x, y = y, x+y
+    for i in range(n-2): x, y = y, x+y
     return y % 1234567
