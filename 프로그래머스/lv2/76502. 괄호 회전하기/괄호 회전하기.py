9fd6b6148de12353ef521ef4f041fb629e6df392
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-15 11:29:29 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,22 @@
+from collections import deque
+def solution(s):
+    l_s = len(s)
+    if l_s%2: return 0
+    q, cnt = deque(s), 0
+    for _ in range(l_s):
+        flag, stack = True, []
+        for i in range(l_s):
+            if q[i] in ['[', '{', '(']: stack = stack + [q[i]]
+            else:
+                if stack == []: 
+                    flag = False
+                    break
+                else:
+                    if (stack[-1] == '[' and q[i] == ']') or (stack[-1] == '{' and q[i] == '}') or (stack[-1] == '(' and q[i] == ')'): stack.pop()
+                    else:
+                        flag = False
+                        break  
+                    
+        if flag and stack == []: cnt += 1
+        q.rotate(-1)
+    return cnt
