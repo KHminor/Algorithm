19b29d61c09af7519405b3f09cc5023f85859b24
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-20 13:48:51 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 617.71 ms, Memory: 65.4 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -2,7 +2,7 @@ def solution(elements):
     dic, ln = {i:0 for i in range(sum(elements)+1)}, len(elements)
     for i in range(ln):
         hap = elements[i]
-        dic[hap] = hap
+        dic[hap] = hap  
         for j in range(i+1, i+ln):
             hap += elements[j%ln]
             if not dic[hap]: dic[hap] = hap            
