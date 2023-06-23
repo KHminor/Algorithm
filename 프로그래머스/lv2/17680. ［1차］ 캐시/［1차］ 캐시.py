9f90e2bbce26e825244962fe32d947e6903d9779
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-23 11:15:15 +0900

    [level 2] Title: [1차] 캐시, Time: 1.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -2,16 +2,14 @@ from collections import deque
 
 def solution(cacheSize, cities):
     if cacheSize == 0: return len(cities)*5 
-
     cities = [ i.upper() for i in cities]
     result, cnt, cnt_idx, cache_li = 0, 0, 0, deque()
-
     while cnt < len(cities):
-        if cities[cnt] in cache_li: 
+        if cities[cnt] in cache_li: # 지역이 존재할 경우 가장 앞에 있는 값을 맨 뒤로 보내면서 +1 해주기
             del cache_li[cache_li.index(cities[cnt])]
             cache_li.append(cities[cnt])
             result += 1
-        else:
+        else: # 없다면
             result += 5
             if len(cache_li) < cacheSize:
                 cache_li.append(cities[cnt])
