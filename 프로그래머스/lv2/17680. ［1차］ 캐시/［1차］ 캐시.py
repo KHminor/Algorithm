7dc61940d0b45f22708debb26d1f34becd873bff
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-23 11:18:41 +0900

    [level 2] Title: [1차] 캐시, Time: 1.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -4,8 +4,8 @@ def solution(cacheSize, cities):
     if cacheSize == 0: return len(cities)*5 
 
     cities = [ i.upper() for i in cities]
-    result, cnt, cnt_idx, cache_li = 0, 0, 0, deque()
-    
+    result, cnt, cnt_idx, cache_li = 0, 0, 0, deque(maxlen=cacheSize)
+
     while cnt < len(cities):
         if cities[cnt] in cache_li: # 지역이 존재할 경우 가장 앞에 있는 값을 맨 뒤로 보내면서 +1 해주기
             del cache_li[cache_li.index(cities[cnt])]
@@ -13,12 +13,8 @@ def solution(cacheSize, cities):
             result += 1
         else: # 없다면
             result += 5
-            if len(cache_li) < cacheSize:
-                cache_li.append(cities[cnt])
-            else:
-                cache_li.rotate(-1)
-                cache_li[-1] = cities[cnt]
-                
+            cache_li.append(cities[cnt])
+            
         cnt, cnt_idx = cnt+1, cnt_idx+1
         if cnt_idx == cacheSize: cnt_idx %= cacheSize
 
