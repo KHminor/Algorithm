630dc98a9f564bcda2a72a3b414159e23149b498
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-26 11:44:46 +0900

    [level 2] Title: 튜플, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -0,0 +1,15 @@
+import re
+
+def solution(s):
+    answer = []
+    a = re.findall(r'\{.*?\}', s[1:-1])
+    a.sort(key=len)
+    a = [set(map(int,i.strip('{}').split(','))) for i in a]
+    if len(a) == 1:
+        answer =  list(a[0])
+    else:
+        for i in range(len(a)-1):
+            if i == 0: 
+                answer.append(list(a[i])[0])
+            answer.append(list(a[i+1].difference(a[i]))[0])
+    return answer
