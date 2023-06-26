a3cbab5914ca44af8a4373e7677d0ad1afa7ef12
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-26 11:56:42 +0900

    [level 2] Title: 튜플, Time: 0.13 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.py
+++ 프로그래머스/lv2/64065. 튜플/튜플.py
@@ -1,13 +1,6 @@
 import re
+from collections import Counter
 
 def solution(s):
-    answer = []
-    li = re.findall(r'\{.*?\}', s[1:-1])
-    li.sort(key=len)
-    li = [set(map(int,i.strip('{}').split(','))) for i in li]
-    if len(li) == 1: answer =  list(li[0])
-    else:
-        for i in range(len(li)-1):
-            if i == 0: answer.append(list(li[i])[0])
-            answer.append(list(li[i+1].difference(li[i]))[0])
-    return answer
+    s = Counter(re.findall('\d+', s))
+    return [int(k) for k,v in sorted(s.items(), key=lambda x:x[1], reverse=True)]
