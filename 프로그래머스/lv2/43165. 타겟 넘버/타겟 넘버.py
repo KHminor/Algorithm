1f1f5e9e405bb01c040e56628baba9afc505a216
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-28 11:27:08 +0900

    [level 2] Title: 타겟 넘버, Time: 4.73 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,8 @@
-from collections import deque
-
 def solution(numbers, target):
     answer = 0
-    stack = deque([[0,0]]) 
+    stack = [[0,0]] 
     while stack:
-        idx, hap = stack.popleft()
+        idx, hap = stack.pop()
         if idx < len(numbers):
             stack.append([idx+1, hap+numbers[idx]])
             stack.append([idx+1, hap-numbers[idx]])
