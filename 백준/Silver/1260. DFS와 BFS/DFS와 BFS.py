25d08e4873cf9a9121ccca66e04feb413ec4df01
Hminor <109326297+KHminor@users.noreply.github.com>
2023-06-28 10:22:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 100 ms, Memory: 34168 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,43 @@
-def dfs(v):
-    visited = [0]*(N+1)
-    stack = [v]
+import sys
+from collections import deque
 
-    while stack:
-        v = stack.pop()
-        if visited[v] == 1:
-            continue
-        print(v, end=' ')
-        visited[v] = 1
-        li = ch[v][:]
-        li.sort(reverse=True)
+n, m, v = map(int,sys.stdin.readline().split())
 
-        for i in li:
-            if visited[i] == 0 :
-                stack.append(i)
+_dict = {i:[] for i in range(n+1)}
+for i in range(m):
+    s,e = map(int,sys.stdin.readline().split())
+    _dict[s].append(e)
+    _dict[e].append(s)
 
-def bfs(v):
-    visitied = [0]*(N+1)
-    q = [v]
+stack, s_dfs, que, q_bfs = [v], [], deque([v]), [] # dfs = stack, s_dfs, bfs = que, q_bfs
+visit = [0]*(n+1) # 방문 체크
 
-    while q:
-        v = q.pop(0)
-        if visitied[v] == 1:
-            continue
-        print(v, end=' ')
+# dfs
+while stack:
+    node = stack.pop()
+    
+    # 방문 체크
+    if visit[node]: continue
+    visit[node] = 1 
 
-        visitied[v] = 1
-        li = ch[v]
-        li.sort()
-        for i in li:
-            if visitied[i] == 0:
-                q.append(i)
+    # stack은 가장 큰 값부터 검사해서, 정점 번호가 작은 것을 먼저 방문하도록 내림차순 정렬
+    for i in sorted(_dict[node], reverse=True): 
+        if not visit[i]: stack.append(i)
+    s_dfs.append(node)
 
-N,M,V = map(int,input().split())
-ch = [[]*(N+1) for _ in range(N+1)]
-t = [0]*(N+1)
-for _ in range(M):
-    p,c = map(int,input().split())
-    ch[p].append(c)
-    ch[c].append(p)
+visit = [0]*(n+1) # 방문 초기화
+# dfs
+while que:
+    node = que.popleft()
 
-dfs(V)
-print()
-bfs(V)
-print()
+    # 방문 체크
+    if visit[node]: continue
+    visit[node] = 1 
+
+    # que는 앞에서부터 pop 시키기 때문에 오름차순 정렬
+    for i in sorted(_dict[node]): 
+        if not visit[i]: que.append(i)
+    q_bfs.append(node)
+
+print(*s_dfs)
+print(*q_bfs)
