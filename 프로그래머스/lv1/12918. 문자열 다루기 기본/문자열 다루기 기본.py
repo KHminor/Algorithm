a013cf9beb921aa59ea82a4efdfcb69af25e48af
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-02 11:19:49 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -2,6 +2,5 @@ def solution(s):
     n_li = [str(i) for i in range(0,10)]
     if not (len(s) == 4 or len(s) == 6): return False 
     for i in s:
-        if i not in n_li:
-            return False
+        if i not in n_li: return False
     return True
