8ae7c48f61f9708e9f3797ed238fcdfe22ba6562
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-03 09:28:52 +0900

    [level 1] Title: 행렬의 덧셈, Time: 20.82 ms, Memory: 23 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,7 +1,2 @@
-def solution(arr1, arr2):
-    ln1, ln2 = len(arr1), len(arr1[0])
-    li = [[0 for i in range(ln2)] for i in range(ln1)]
-    for i in range(ln1):
-        for j in range(ln2):
-            li[i][j] = arr1[i][j] + arr2[i][j]
-    return li
+def solution(A,B):
+    return [list(map(sum, zip(*x))) for x in zip(A, B)]
