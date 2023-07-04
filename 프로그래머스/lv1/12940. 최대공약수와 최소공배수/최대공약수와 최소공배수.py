834c3b4bb089fcede3ae3802a397a95513456b5f
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-04 09:22:55 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,11 +1,4 @@
-def gcd(n,m):
-    while m != False:
-        n,m = m, n%m    
-    return n
-
-def lcm(n,m,x):
-    return n*m//x
-
 def solution(n, m):
-    x = gcd(n,m) 
-    return [x,lcm(n,m,x)]
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
