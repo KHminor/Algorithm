3c9499a9e0f5e958731538004d3ff3e25099e720
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-05 10:26:13 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,7 +1,8 @@
-def solution(number):
-    ln, cnt = len(number), 0
-    for i in range(ln-2):
-        for j in range(i+1, ln-1):
-            for z in range(j+1, ln):
-                if number[i]+number[j]+number[z] == 0: cnt+= 1
+from itertools import combinations
+
+def solution (number) :
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
     return cnt
