f0e03461f5a41a301fa9cf1c9adf67e2fe0a68da
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-07 09:31:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    result, s = '', list(s.split(" "))
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            if not j%2: result+= s[i][j].upper()
+            else: result += s[i][j].lower()
+        if i != len(s)-1: result += ' '
+    return result
