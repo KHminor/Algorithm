4070a30fb26e95f154fa7036515892ad1cee9ca6
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-26 11:22:37 +0900

    [Silver V] Title: 셀프 넘버, Time: 216 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,13 +1,5 @@
-def d(n):
-    num_li = list(map(int,str(n)))
-    num = sum(num_li)+n
-    return num
-
-result = [i for i in range(1,10000)]
-for i in range(1,10000):
-    num = d(i)
-    if num in result:
-        result.remove(num)
-
-for x in result:
-    print(x)
+result = [i for i in range(1, 10001)]
+for i in range(1, 10001):
+    hap = i + sum(list(map(int,str(i))))
+    if hap in result: result.remove(hap)
+for i in result: print(i)
