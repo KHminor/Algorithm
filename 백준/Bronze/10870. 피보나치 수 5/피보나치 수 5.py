1babc5cbee656365b1f18af17cc4d38566903b44
Hminor <109326297+KHminor@users.noreply.github.com>
2023-07-29 11:47:24 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,16 +1,5 @@
-import sys
-
-def fibo(s,n):
-    if n == 0:
-        return 0
-    elif s == n+1:
-        return li[n]
-    else:
-        li.append(li[s - 2] + li[s - 1])
-        return fibo(s+1,n)
-
-
-
-li = [0,1]
-num = int(sys.stdin.readline())
-print(fibo(2,num))
+n = int(input())
+li = [ i if i == 1 else 0 for i in range(n+1)]
+for i in range(2, n+1):
+    li[i] = li[i-2] + li[i-1]
+print(li[-1])
