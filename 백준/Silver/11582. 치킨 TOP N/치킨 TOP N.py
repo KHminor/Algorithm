e8aa3647ef43d672cc7797ab26152f49762b7f85
Hminor <109326297+KHminor@users.noreply.github.com>
2023-08-10 12:05:56 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 1076 ms, Memory: 147496 KB -BaekjoonHub

--- 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
@@ -1,20 +1,8 @@
-def CTP(li):
-    global k, n
-    if len(li) == 1: return li
-    mid = len(li)//2
-    left, right = CTP(li[:mid]), CTP(li[mid:])
-    if k == n//mid: result.append(left+right)
-    return sorted(left+right)
-
-
 import sys
 input = sys.stdin.readline
 n = int(input())
 li = list(map(int,input().split()))
 k = int(input())
-result = []
-if k == n: print(*li)
-elif k == 1: print(*sorted(li))    
-else: 
-    CTP(li)
-    for i in result: print(*i, end=' ')
+
+idx = n//k
+for i in range(0, n, idx): print(*sorted(li[i:i+idx]), end=' ')
