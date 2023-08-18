377e504d4f716f7fd2f5cbd80f8a721824980e12
Hminor <109326297+KHminor@users.noreply.github.com>
2023-08-18 11:45:57 +0900

    [Silver I] Title: 효율적인 해킹, Time: 12668 ms, Memory: 215484 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,28 @@
+def bfs(i):
+    q = deque([i])
+    visit[i] = True
+    while q:
+        li[i] += 1 
+        s = q.popleft()
+        for x in arr[s]:
+            if not visit[x]:
+                visit[x] = True
+                q.append(x)
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+n,m = map(int,input().split())
+arr = [[] for _ in range(n+1)]
+li = [0]*(n+1)
+mx = 0
+for _ in range(m):
+    a,b = map(int,input().split())
+    arr[b].append(a)
+
+for i in range(1,n+1):
+    visit = [False]*(n+1)
+    bfs(i)
+    if li[i] > mx: mx = li[i]
+
+[print(i, end=' ') for i in range(1,n+1) if li[i] == mx]
