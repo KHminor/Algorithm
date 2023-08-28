a3e40d2523c8faa37a35b7206c53a314ba717103
Hminor <109326297+KHminor@users.noreply.github.com>
2023-08-28 09:46:28 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,6 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-result = 0
-for i in sorted(map(int,input().rstrip('\n').split())):
-    result += n*i
-    n -= 1
-print(result)
+li = sorted(map(int,input().rstrip('\n').split()))
+for i in range(1,n): li[i] += li[i-1]
+print(sum(li))
