93c5948dc08325c59201fa6d9c98aa4734936755
Hminor <109326297+KHminor@users.noreply.github.com>
2023-08-29 10:22:12 +0900

    [Silver IV] Title: 듣보잡, Time: 92 ms, Memory: 44492 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,6 +1,8 @@
+import sys
+input = sys.stdin.readline
 n,m = map(int,input().split())
-no_listen = set([input() for _ in range(n)])
-no_show = set([input() for _ in range(m)])
+no_listen = set([input().rstrip('\n') for _ in range(n)])
+no_show = set([input().rstrip('\n') for _ in range(m)])
 result = sorted(no_listen.intersection(no_show))
 print(len(result))
 [print(i) for i in result]
