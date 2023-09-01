31431b459108859a5a1a13956357f789bc0e5c42
Hminor <109326297+KHminor@users.noreply.github.com>
2023-09-01 10:16:47 +0900

    [Bronze I] Title: 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -3,9 +3,8 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     a,b = map(int,input().rstrip('\n').split())
-    c_a,c_b = a,b
-    
-    while c_a != c_b:
-        if c_a > c_b: c_b += b
-        else: c_a += a
-    print(c_a)
+    result = a*b
+    if b>a: a,b = b,a
+    while b != 0:
+        a,b = b, a%b
+    print(result//a)
