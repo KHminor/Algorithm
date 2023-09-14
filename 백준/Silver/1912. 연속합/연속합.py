7fd0eb0d91c8346ea5db55a6c87495a3d3e07d06
Hminor <109326297+KHminor@users.noreply.github.com>
2023-09-14 11:01:43 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,10 +1,10 @@
 import sys
+
 input = sys.stdin.readline
 n = int(input())
-li = list(map(int,input().split()))
+li = list(map(int,input().rstrip('\n').split()))
 dp = [0]*n
 dp[0] = li[0]
 
-for i in range(1, n):
-    dp[i] = max(li[i]+dp[i-1], li[i])
+for i in range(1,n): dp[i] = max(dp[i-1]+li[i], li[i])
 print(max(dp))
