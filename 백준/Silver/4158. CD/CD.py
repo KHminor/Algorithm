23cd2cc3fac60bba86fc00f9de4d19f2fe49e22c
Hminor <109326297+KHminor@users.noreply.github.com>
2023-09-22 11:04:16 +0900

    [Silver V] Title: CD, Time: 1656 ms, Memory: 136120 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -4,8 +4,17 @@ input = sys.stdin.readline
 while True:
     n,m = map(int,input().split())
     if (n,m) != (0,0):
-        _n,_m = set(),set()
-        for _ in range(n): _n.add(int(input()))
-        for _ in range(m): _m.add(int(input()))
-        print(len(_n.intersection(_m)))
+        cnt = 0
+        li = [int(input()) for _ in range(n)] # 1 2 3
+        for _ in range(m):
+            find = int(input()) # 1 2 4
+            s,e = 0,n-1
+            while e>=s:
+                mid = (s+e)//2
+                if li[mid] == find: 
+                    cnt += 1
+                    break
+                elif li[mid] > find: e = mid-1
+                else: s = mid+1
+        print(cnt)
     else: break
