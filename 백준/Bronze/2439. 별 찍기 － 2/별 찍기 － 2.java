e5e4f51b13e7bcecdf043b9fb1f55d57d72a40db
Hminor <109326297+KHminor@users.noreply.github.com>
2023-10-14 11:33:00 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 244 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < a-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
