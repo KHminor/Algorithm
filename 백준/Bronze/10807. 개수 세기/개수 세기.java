ea45d5f94233484ce54e5dc1aeb640cad02f58eb
Hminor <109326297+KHminor@users.noreply.github.com>
2023-10-15 10:45:24 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -4,9 +4,9 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        int v = Integer.parseInt(br.readLine());
         int result = 0;
         for (int i = 0; i < n; i++) {
             if (v == Integer.parseInt(st.nextToken())) result += 1;
