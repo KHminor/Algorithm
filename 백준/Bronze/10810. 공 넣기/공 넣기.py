f4b7662d9dcdf3ca39679c1f3687a140b2ab908c
Hminor <109326297+KHminor@users.noreply.github.com>
2023-12-11 09:54:19 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,9 +1,9 @@
 import sys
-n,m = map(int,sys.stdin.readline().split())
-li = [0 for _ in range(n+1)]
+input = sys.stdin.readline
 
+n,m = map(int,input().split())
+li = [0]*(n+1)
 for _ in range(m):
-    i,j,k = map(int,sys.stdin.readline().split())
-    for i in range(i,j+1):
-        li[i] = k
+    i,j,k = map(int,input().split())
+    for x in range(i,j+1): li[x] = k
 print(*li[1:])
