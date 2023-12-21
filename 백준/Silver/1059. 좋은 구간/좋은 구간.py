59fb4b17d7cfeed76fe062a93195c3088de6a213
Hminor <109326297+KHminor@users.noreply.github.com>
2023-12-21 10:43:33 +0900

    [Silver IV] Title: 좋은 구간, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -1,17 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 L = int(input())
-nums = list(map(int, input().split()))
-target = int(input()) # == n
+S = list(map(int,input().split()))
+n = int(input())
+S.append(0)
+S.sort()
 
-nums.append(0)
-nums.sort()
 
-A = 0
-for i in range(len(nums)-1) :
-    if nums[i] == target or nums[i+1] == target:
-        A = 0
+for i in range(len(S)-1):
+    if S[i] == n or n == S[i+1]:
+        print(0)
         break
-    elif nums[i] < target and target < nums[i+1]:
-        A = (target - nums[i]) * (nums[i+1] - target) - 1
+    elif S[i] <= n <= S[i+1]:
+        print((n-S[i])*(S[i+1]-n)-1)
         break
-
-print(A)
