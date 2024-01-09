9d29fd1efb1dde4043b72d8e620f2fcb068f5f6a
Hminor <109326297+KHminor@users.noreply.github.com>
2024-01-09 11:45:49 +0900

    [Bronze II] Title: 타임머신, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1440. 타임머신/타임머신.py
+++ 백준/Bronze/1440. 타임머신/타임머신.py
@@ -1,8 +1,8 @@
 from itertools import permutations
 li = list(input().split(":"))
-result = []
+result = 0
 for i in permutations(li):
     x = list(i)
     h,m,s = int(x[0]),int(x[1]),int(x[2])
-    if 0<h<=12 and 0<=m<=59 and 0<=s<=59: result.append([x[0],x[1],x[2]])
-print(len(result))
+    if 0<h<=12 and 0<=m<=59 and 0<=s<=59: result+=1
+print(result)
