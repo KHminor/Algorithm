6436c5cd04bc937c71fe11f3177b499e4b092ae8
Hminor <109326297+KHminor@users.noreply.github.com>
2024-01-15 23:03:17 +0900

    [level Lv.3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(begin, target, words):
+    if target not in words: return 0
+    w_ln = len(words)  
+    que = deque([[begin,0]]) 
+
+    while True:
+        b, idx = que.popleft() 
+        visit, flag = [0]*w_ln, False  
+        if b == target: break 
+
+        for i in range(w_ln):
+            for j in range(len(words[i])):  
+                if b[j] == words[i][j]: visit[i] += 1
+        for i in range(w_ln):
+            if len(begin)-1 == visit[i]: 
+                if words[i] == target: 
+                    idx += 1
+                    flag = True
+                    break 
+                que.append([words[i], idx+1])
+                words[i] = ''
+        if flag: break
+
+    return idx
