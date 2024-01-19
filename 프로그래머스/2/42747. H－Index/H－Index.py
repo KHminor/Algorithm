c65c8bbc1316ede6ccfbc7b210168b592e91d9d6
Hminor <109326297+KHminor@users.noreply.github.com>
2024-01-19 23:53:44 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    h, c_ln, result = 0, len(citations), 0
+    for i in range(1, c_ln+1):
+        ln = len([j for j in citations if j >= i])
+        if not (ln >= i): break
+        elif ln >= i: result = i  
+    return result
