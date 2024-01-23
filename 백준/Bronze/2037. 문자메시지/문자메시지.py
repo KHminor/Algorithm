c00d1195cd8242a938af048271e507f647bb9138
Hminor <109326297+KHminor@users.noreply.github.com>
2024-01-23 10:59:57 +0900

    [Bronze I] Title: 문자메시지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2037. 문자메시지/문자메시지.py
+++ 백준/Bronze/2037. 문자메시지/문자메시지.py
@@ -1,20 +1,29 @@
-if __name__ == '__main__':
-    keyboard = [[], ['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'], ['M', 'N', 'O'],
-                ['P', 'Q', 'R', 'S'], ['T', 'U', 'V'], ['W', 'X', 'Y', 'Z']]
-    p, w = map(int, input().split())
-    text = input()
+p,w = map(int,input().split())
+st = input()
+phone = [
+    [" "], 
+    ["A","B","C"], 
+    ["D","E","F"], 
+    ["G","H","I"], 
+    ["J","K","L"], 
+    ["M","N","O"], 
+    ["P","Q","R","S"], 
+    ["T","U","V"], 
+    ["W","X","Y","Z"]
+    ]
 
-    total = 0
-    past_j = ''
-    for i in range(len(text)):
-        if text[i] == ' ':
-            total += p
-            past_j = ''
-        for j in range(len(keyboard)):
-            for k in range(len(keyboard[j])):
-                if text[i] == keyboard[j][k]:
-                    if j == past_j:
-                        total += w
-                    total += (1+k) * p
-                    past_j = j
-    print(total)
+before = ""
+result = 0
+for i in st:
+    if i == " ": 
+        result += p
+        before = ""
+        continue
+    for j in range(9):
+        if i in phone[j]:
+            num = (phone[j].index(i)+1)*p
+            if before == j: result += (num+w)
+            else: result += num
+            before = j
+            break
+print(result)
