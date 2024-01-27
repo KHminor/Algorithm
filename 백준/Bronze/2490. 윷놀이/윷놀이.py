a543458496c477bf1aad36af7686952784159827
Hminor <109326297+KHminor@users.noreply.github.com>
2024-01-27 11:52:56 +0900

    [Bronze III] Title: 윷놀이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -6,6 +6,5 @@ for _ in range(3):
     for i in list(map(int,input().split())):
         if i == 0: o +=1
         else: t += 1
-    if o == 4: print("D")
-    elif t == 4: print("E")
+    if t == 4: print("E")
     else: print(chr(64+o))
