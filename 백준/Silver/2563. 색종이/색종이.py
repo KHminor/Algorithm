7fc9da1f54cd2dd069810da180ade037ffcdf4fb
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-08 10:52:24 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,15 +1,15 @@
 import sys
-paper = int(sys.stdin.readline())
-arr = [[0]*100 for _ in range(100)]
-cnt = 0
-for _ in range(paper):
-    si,sj = map(int,sys.stdin.readline().split())
-    ei,ej = si+10,sj+10
+input = sys.stdin.readline
 
-    for i in range(si,ei):
-        for j in range(sj,ej):
-            if arr[i][j] == 0:
-                arr[i][j] = 1
-                cnt +=1
+mtx = [[0]*100 for _ in range(100)]
 
-print(cnt)
+result = 0
+for _ in range(int(input())):
+    x,y = map(int,input().split())
+    for i in range(y, y+10):
+        for j in range(x,x+10):
+            if not mtx[i][j]:
+                mtx[i][j] = 1
+                result += 1
+
+print(result)
