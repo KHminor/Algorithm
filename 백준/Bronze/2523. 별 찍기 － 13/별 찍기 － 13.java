b08ecd2c47cadf934fb46c70bd660f2443505192
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-13 10:53:12 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 220 ms, Memory: 12956 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < 2*n; i++) {
+            if (i <= n) for (int j = 0; j < i; j++) System.out.print("*");
+            else for (int j = 0; j < 2 * n - i; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
