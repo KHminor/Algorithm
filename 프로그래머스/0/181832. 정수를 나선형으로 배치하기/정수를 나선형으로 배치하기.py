d03c8d7203425f23e9580922154bab41a30e07a2
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-16 11:41:20 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.15 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -0,0 +1,34 @@
+def solution(n):
+    mtx = [[0]*n for _ in range(n)]
+    i,vi,j,vj = 0,n-1,0,n-1
+    
+    s = 1
+    r = 0
+    while s <= n*n:
+        while j <= vj and mtx[i][j] == 0:
+            mtx[i][j] = s
+            s += 1
+            j += 1
+        i,j = i+1,j-1
+
+        while i <= vi and mtx[i][j] == 0:
+            mtx[i][j] = s
+            s += 1
+            i += 1
+        i,j = i-1,j-1
+
+        while j >= r and mtx[i][j] == 0:
+            mtx[i][j] = s
+            s += 1
+            j -= 1
+        i,j = i-1,j+1
+
+        while j >= r and mtx[i][j] == 0:
+            mtx[i][j] = s
+            s += 1
+            i -= 1
+        i,j = i+1,j+1
+
+        vi,vj = vi-1,vj-1
+        r += 1
+    return mtx
