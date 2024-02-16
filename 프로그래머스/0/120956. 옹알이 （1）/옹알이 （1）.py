fb9c6e61ab147d4a04f9a183987c742fc1a3c940
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-16 10:32:09 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,24 @@
+def solution(babbling):
+    answer = 0
+    
+    for i in range(len(babbling)):
+        b_ln = len(babbling[i])
+        if b_ln < 2: continue
+        word = {"aya", "ye", "woo", "ma"}
+        s = 0
+        state = True
+        while b_ln > s:
+            if b_ln >= s+3 and babbling[i][s:s+3] in word: 
+                print(babbling[i][s:s+3])
+                word.remove(babbling[i][s:s+3])
+                s += 3
+            elif babbling[i][s:s+2] in word: 
+                print(babbling[i][s:s+2])
+                word.remove(babbling[i][s:s+2])
+                s += 2
+            else:
+                state = False
+                break
+        if state:
+            answer += 1
+    return answer
