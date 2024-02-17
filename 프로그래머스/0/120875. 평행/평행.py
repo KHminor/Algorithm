faf682d0fb447cee6087c67897ff8ceafa067d5d
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-17 11:59:10 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,10 @@
+def solution(dots):
+    def slope(first, second):
+        return (second[1] - first[1]) / (second[0] - first[0])
+    for i in range(3):
+        mydots = dots[:]
+        if slope(mydots.pop(), mydots.pop(i)) == slope(mydots.pop(), mydots.pop()):
+            return 1
+    return 0
+
+print(solution([[3, 5], [4, 1], [2, 4], [5, 10]]))
