4395678ea48ceaf76de81724303dcc02a2a625b7
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-20 12:03:46 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,9 @@
+def solution(ingredient):
+    result = 0
+    check = []
+    for i in ingredient:
+        check.append(i)
+        if check[-4:] == [1,2,3,1]:
+            result += 1
+            del check[-4:]
+    return result 
