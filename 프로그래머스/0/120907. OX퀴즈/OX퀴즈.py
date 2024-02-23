8bbbf43964395e179ead6c4adf95ce9936642a7d
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-23 10:29:35 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.py
@@ -1,9 +1,1 @@
-def solution(quiz):
-    result = []
-    for i in quiz:
-        li = i.replace("=","").split()
-        if li[1] == "+" and int(li[0])+int(li[2]) == int(li[-1]): result.append("O")
-        elif li[1] == "-" and int(li[0])-int(li[2]) == int(li[-1]): result.append("O")
-        else: result.append("X")
-            
-    return result
+def solution(quiz): return ["O" if eval(i.replace("=","==")) else "X" for i in quiz]
