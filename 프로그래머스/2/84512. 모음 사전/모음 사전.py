6c1b197a5446cae4396e623ca82a88c161e08daa
Hminor <109326297+KHminor@users.noreply.github.com>
2024-02-29 11:56:27 +0900

    [level 2] Title: 모음 사전, Time: 0.64 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,18 @@
+cnt = 0
+
+def solution(word):
+    global cnt
+    li = ['']*5
+    
+    def find(idx): 
+        global cnt   
+        if "".join(li) == word: return cnt
+        elif idx == 5: return
+        else:
+            for i in ['A','E','I','O','U']:
+                cnt += 1
+                li[idx] = i
+                if find(idx+1): return cnt
+                li[idx] = ''
+    cnt = 0
+    return find(0)
