40e065942aed66865b89c79cb7d64fca062a5d13
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-01 21:50:09 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.46 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,7 @@
+def solution(s):
+    binary_cnt = zero_cnt = 0
+    while s != '1': 
+        zero_cnt += s.count('0')
+        s = bin(len(s.replace('0', '')))[2:]
+        binary_cnt += 1 
+    return [binary_cnt, zero_cnt]
