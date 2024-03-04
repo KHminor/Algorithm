7adb9c41a991118b2843dbd2aa141d5ca552f16e
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-04 11:03:13 +0900

    [level 0] Title: 배열 조각하기, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -1,5 +1,5 @@
 def solution(arr, query):
-    for i in range(len(query)):
-        if not i%2: arr = arr[:query[i]+1]
-        else: arr = arr[query[i]:]
+    for i,q in enumerate(query):
+        if not i%2: arr = arr[:q+1]
+        else: arr = arr[q:]
     return arr
