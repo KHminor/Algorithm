1a446f7f86cfd9045627504d69d6906ed6cef3e2
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-07 10:41:23 +0900

    [level 1] Title: 소수 찾기, Time: 2611.46 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,6 +1,11 @@
 def solution(n):
-    li = [1]*(n+1)
-    for i in range(2,n+1):
-        if li[i] != 0:
-            for j in range(2*i,n+1,i): li[j] = 0
-    return sum(li[2:])
+    result = [2]
+    for i in range(3,n+1):
+        state = True
+        rng = round(i**(1/2))
+        for j in range(2,rng+1):
+            if not i%j:
+                state = False
+                break
+        if state: result.append(i)
+    return len(result)
