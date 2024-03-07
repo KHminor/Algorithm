819bb571e97fd22e0df6b0dc92484094c3351544
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-07 11:55:24 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,5 @@
+def solution(x,y):
+    xy = set(x) & set(y)
+    if not xy: return "-1"
+    elif len(xy) == 1 and "0" in xy: return "0"
+    return "".join(sorted([i*min(x.count(i),y.count(i)) for i in xy], reverse=True))
