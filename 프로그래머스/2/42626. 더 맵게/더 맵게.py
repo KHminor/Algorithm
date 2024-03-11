1feaa56cf4afb861f432b2707d57a085bdd00237
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-11 12:20:29 +0900

    [level 2] Title: 더 맵게, Time: 1740.65 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,20 @@
+import heapq
+
+def solution(s, K):
+    state = True
+    for i in s:
+        if K > i: 
+            state = False
+            break 
+    if state: return 0
+
+    result = 0
+    s.sort()
+    heapq.heapify(s)
+
+    while len(s) >= 2:
+        heapq.heappush(s,heapq.heappop(s)+heapq.heappop(s)*2)
+        result += 1
+        if s[0] >= K: return result
+
+    return -1
