418c0380b2ff4e24056cb360d2d090328c838104
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-11 10:40:46 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,8 @@
+def solution(k, m, score):
+    result,cnt = 0,0
+    for i in sorted(score,reverse=True):
+        cnt += 1
+        if cnt == m: 
+            result += i*m
+            cnt = 0
+    return result
