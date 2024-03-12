a095ec963300a0fa109a8c9a1f2d909d92e09267
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-12 09:52:25 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -2,4 +2,4 @@ from datetime import datetime
 
 def solution(a, b):
     weekend = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
-    return weekend[(datetime.strptime("2016-%d-%d"%(a,b),"%Y-%m-%d").weekday()+1)%7]
+    return weekend[(datetime(2016,a,b).weekday()+1)%7]
