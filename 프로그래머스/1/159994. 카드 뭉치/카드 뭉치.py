ff1d08c4f60f436f58828ac74e3c58d88d780a3f
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-12 10:55:50 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,8 +1,6 @@
 def solution(cards1, cards2, goal):
     for g in goal:
-        if cards1 and g == cards1[0]: 
-            del cards1[0]
-        elif cards2 and g == cards2[0]: 
-            del cards2[0]
+        if cards1 and g == cards1[0]: del cards1[0]
+        elif cards2 and g == cards2[0]: del cards2[0]
         else: return "No"
     else: return "Yes"
