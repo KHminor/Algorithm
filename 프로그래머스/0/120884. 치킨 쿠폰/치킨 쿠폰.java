72b3013c92422494dd6f80c36546c9e50a305f33
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-15 17:02:55 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int chicken) {
-        int result = 0;
-        while (chicken >= 10) {
-            int dack = chicken/10;
-            result += dack;
-            chicken = chicken%10+dack;
+        int answer = chicken/9;
+        if(chicken>1&&chicken%9==0){
+            answer--;
         }
-        return result;
+
+
+        return answer;
     }
 }
