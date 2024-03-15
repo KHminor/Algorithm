e0ad03d8acb95f8341c468f75fdf7479b6189dce
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-15 16:22:29 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,16 +1,9 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        boolean is_id = false;
-        boolean is_pwd = false;
         for (int i=0; i < db.length; i++) {
             if (db[i][0].equals(id_pw[0])&& db[i][1].equals(id_pw[1])) return "login";
-            else if (db[i][0].equals(id_pw[0])) is_id = true;
-            else if (db[i][1].equals(id_pw[1])) is_pwd = true;
+            else if (db[i][0].equals(id_pw[0])) return "wrong pw";
         }
-        
-        if (is_id) {
-          return "wrong pw";
-        } 
         return "fail";
     }
 }
