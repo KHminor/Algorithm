a6abb69e28512d481008a89edac00796a2484d67
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-16 11:36:09 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,8 +1,9 @@
+import java.util.*;
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        char[] li = my_string.toCharArray();
-        for (int i =my_string.length()-1; i>=0; i--) answer+= li[i]; 
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+        return sb.toString();
     }
 }
