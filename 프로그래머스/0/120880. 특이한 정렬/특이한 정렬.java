7b7b6262a26c91fc942bbf97297e6bfaf59c7c40
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-19 10:06:44 +0900

    [level 0] Title: 특이한 정렬, Time: 2.20 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        HashMap<Integer,ArrayList<Integer>> dic = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int num: numlist) {
+            int x = Math.abs(num-n);
+            if (dic.containsKey(x)) {
+                ArrayList<Integer> li = dic.get(x);
+                li.add(num);
+            }
+            else {
+                ArrayList<Integer> li = new ArrayList<Integer>();
+                li.add(num);
+                dic.put(x,li);
+            }
+        }
+        
+        ArrayList<Integer> s_li = new ArrayList<>(dic.keySet());
+        Collections.sort(s_li);
+        for (Integer key: s_li) {
+            if (dic.get(key).size() != 1) {
+                Collections.sort(dic.get(key));
+                Collections.reverse(dic.get(key));
+            }
+            ArrayList<Integer> li = dic.get(key);
+            for (Integer num: li) {
+                result.add(num);
+            }
+            
+        }   
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
