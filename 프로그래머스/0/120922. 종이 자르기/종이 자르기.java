d33d442dd17f677f2211e5dd9471fdebb771ea6e
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-20 10:39:46 +0900

    [level 0] Title: 종이 자르기, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        int mx = Math.max(M,N);
-        int mn = Math.min(M,N);
-        return (mx-1)*mn+mn-1;
+        return M*N-1;
     }
 }
