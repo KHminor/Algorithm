f5aa2518250d5640b673057efb7ae03b192d4251
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-22 11:19:22 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -5,8 +5,7 @@ def solution(x, y, n):
     q = deque([[x+n,1],[x*2,1],[x*3,1]])
     while q:
         val,cnt = q.popleft()
-        if val > y: continue
-        elif val == y: return cnt
+        if val == y: return cnt
         for i in [val+n,val*2,val*3]:
             if y >= i and i not in visit:
                 visit.add(i) 
