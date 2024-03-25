f3ae734115335bd916e86da9489adc60681eba06
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-25 10:55:54 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,3 @@
-function solution(s1, s2) {
-    const one = new Set(s1)
-    const two = new Set(s2)
-    
-    return ([...s1].filter((val)=>two.has(val))).length;
+function solution(s1, s2) {    
+    return s1.length+s2.length - Array.from(new Set(s1.concat(s2))).length;
 }
