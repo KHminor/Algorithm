c8d377ad991c9bf89aecce72c2787c31d191a9fe
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-29 11:48:51 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,7 +1,5 @@
 def solution(score):
     li = [sum(i)/2 for i in score]
-    result = [len(score)]*len(score)
-    for i in range(len(li)):
-        for j in range(len(li)):
-            if i!=j and li[i] >= li[j]: result[i]-=1
-    return result
+    s_li = sorted(li,reverse=True)
+    
+    return [s_li.index(i)+1 for i in li ]
