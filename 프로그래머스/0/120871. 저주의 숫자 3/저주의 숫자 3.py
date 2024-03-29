43c9abc0ffddbcb17b8888293750544c56880688
Hminor <109326297+KHminor@users.noreply.github.com>
2024-03-29 10:52:40 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,6 +1,6 @@
 def solution(n):
     result = 0
-    for _ in range(1,n+1):
+    for _ in range(n):
         result += 1
         while (not result%3 or "3" in set(str(result))): result += 1
     return result
