93f5e85ca0ae19645c33dbf53b6b9cf98ac37db8
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-02 11:39:09 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,2 +1,2 @@
 def solution(my_string):
-    return "".join([i.lower() if 65<=ord(i)<=90 else i.upper()  for i in my_string])
+    return "".join([i.lower() if i.isupper() else i.upper()  for i in my_string])
