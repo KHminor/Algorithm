3b66fd94214184fe0526a9d22252f6d0a531bc11
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-06 19:18:32 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -6,7 +6,7 @@ def solution(babbling):
         if b_ln < 2: continue
         word = {"aya", "ye", "woo", "ma"}
         s = 0 
-        state = True
+        state = True 
         while b_ln > s:
             if b_ln >= s+3 and babbling[i][s:s+3] in word: 
                 print(babbling[i][s:s+3])
