587d430204f165bba457261e2c46e7994ed6e8c7
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-10 10:55:41 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,8 +1,11 @@
+# def solution(my_string):
+#     li = my_string.split()
+#     result = int(li[0])
+#     oper = "+"
+#     for i in range(1,len(li)):
+#         if li[i] in {"+","-"}: oper = li[i]
+#         else: result = result + int(li[i]) if oper == "+" else result - int(li[i]) 
+#     return result
+
 def solution(my_string):
-    li = my_string.split()
-    result = int(li[0])
-    oper = "+"
-    for i in range(1,len(li)):
-        if li[i] in {"+","-"}: oper = li[i]
-        else: result = result + int(li[i]) if oper == "+" else result - int(li[i]) 
-    return result
+    return int(eval(my_string))
