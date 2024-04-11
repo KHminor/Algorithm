80ca17f2064944fcb170ea4369127f829d726b1d
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-11 15:25:45 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,10 +1,2 @@
 def solution(my_string):
-    result,num = 0,[]
-    li = {str(i) for i in range(10)}
-    for i in my_string:
-        if i in li: num.append(i)
-        elif num != []: 
-            result += int("".join(num))
-            num.clear()
-    if num != []: result += int("".join(num))
-    return result
+    return sum([int(i) for i in "".join([i if i.isdigit() else " " for i in my_string]).split()])
