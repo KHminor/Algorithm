1cc0491472d4db258f47ee50a7b7ac24b64a58b3
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-11 16:01:25 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -1,8 +1,3 @@
-from collections import Counter
 def solution(before, after):
-    a,b = Counter(before),Counter(after)
-    result = 1
-    for i in set(after):
-        if a.get(i) != b.get(i): result = 0
-    return result
+    return 1 if sorted(before) == sorted(after) else 0
     
