aa5e62d0db832b38eb5bf61431af07ccec12ccb0
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-12 11:39:05 +0900

    [level 0] Title: 이진수 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.py
@@ -1,13 +1,2 @@
-# def solution(bin1, bin2):
-#     result,num = [], int(bin1)+int(bin2)
-#     be = 0
-#     while num != 0:
-#         moc,na = divmod(num,10)
-#         num = moc
-#         result.append(str((na+be)%2))
-#         if na+be >= 2: be = 1
-#     if be == 1: result.append("1")    
-#     return "".join(result[::-1])
-
 def solution(bin1, bin2):
     return str(bin(int(bin1,2)+int(bin2,2)))[2:]
