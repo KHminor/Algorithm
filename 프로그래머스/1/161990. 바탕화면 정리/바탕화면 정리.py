d733214d42063d0cfd1c06642c620882b6e54281
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-14 11:58:23 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -3,6 +3,6 @@ def solution(wallpaper):
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[i])):
             if wallpaper[i][j] == "#": 
-                li[0].append(i)
+                li[0].append(i) 
                 li[1].append(j)
     return [min(li[0]),min(li[1]),max(li[0])+1,max(li[1])+1]
