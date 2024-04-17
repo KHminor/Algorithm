988dd19eb332c46cf41fca9b7ec557f4d63a3f89
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-17 11:50:26 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -2,7 +2,7 @@ def solution(players, callings):
     dic = {players[i]:i for i in range(len(players))}
     for i in callings:
         idx = dic[i]
-        dic[i] -= 1
+        dic[i] -= 1 
         dic[players[idx-1]] += 1 
         players[idx],players[idx-1] = players[idx-1],players[idx]
     return sorted(dic,key=dic.get)
