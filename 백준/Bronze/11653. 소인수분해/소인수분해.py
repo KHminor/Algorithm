d9b72ada26e9bb2bb6ebaba5aaea54df2aa0e1db
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-18 10:38:39 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,9 +1,6 @@
 n = int(input())
-num = 2
-result = []
-while n >= num:
-    if not n%num: 
-        n //= num
-        result.append(num)
-    else: num += 1
-[print(i) for i in result]
+for i in range(2, int(n**0.5)+1):
+    while not n%i:
+        n //= i
+        print(i)
+if n != 1: print(n)
