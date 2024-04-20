44be3db7e1e837895bced8b3fc6732b89ee5bda4
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-20 13:19:34 +0900

    [Bronze III] Title: TGN, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    r,e,c = map(int,input().split())
+    if e-c > r: print("advertise")
+    elif e-c == r: print("does not matter")
+    else: print("do not advertise")
