b03a926f8b8ea2928b39dbddbcb72a36b7291182
Hminor <109326297+KHminor@users.noreply.github.com>
2024-04-30 15:39:04 +0900

    [Silver II] Title: 촌수계산, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -3,23 +3,20 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-li1 = [-1 for _ in range(n+1)]
-li2 = [[] for _ in range(n+1)]
+li = [[] for _ in range(n+1)]
 a,b = map(int,input().split())
-v1,v2 = {a},{b}
 
 for _ in range(int(input())):
     x,y = map(int,input().split())
     # 부모에서 자식으로 연결시키기
-    li1[y] = x
-    li2[x].append(y)
+    li[x].append(y)
 
 result = -1
 for i in range(1,n+1):
-    if li2[i]:
+    if li[i]:
         hap = 0
         q = deque([[i,0]])
-        # q = deque([[j,1] for j in li2[i]])
+        # q = deque([[j,1] for j in li[i]])
         s_a,s_b = False, False
         while q:
             num,cnt = q.popleft() # [2,0]
@@ -31,7 +28,7 @@ for i in range(1,n+1):
                 hap += cnt
             if s_a and s_b: break
 
-            for j in li2[num]: q.append([j,cnt+1])
+            for j in li[num]: q.append([j,cnt+1])
         if s_a and s_b:
             if result == -1: result = hap
             else: result = min(result, hap)
