724dbd391d8f181ff0c49539ce64a82f29a1e5ef
Hminor <109326297+KHminor@users.noreply.github.com>
2024-05-20 21:00:02 +0900

    [level 0] Title: 문자열 출력하기, Time: 184.64 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         System.out.println(sc.next());
     } 
 }
