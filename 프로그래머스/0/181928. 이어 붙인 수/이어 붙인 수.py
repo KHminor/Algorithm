89cd24f2214574658a2baa343cf0d2788a59ed1e
Hminor <109326297+KHminor@users.noreply.github.com>
2024-06-18 11:14:17 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,6 +1,6 @@
 def solution(num_list):
-    a,b = [],[]
+    a,b = 0,0
     for i in num_list:
-        if i%2: a.append(str(i))
-        else: b.append(str(i))
-    return int("".join(a)) + int("".join(b))
+        if i%2: a = a*10+i
+        else: b = b*10+i
+    return a + b
