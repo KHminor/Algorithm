0d75e886675b94a9f3e7c904de56c6b71b1347f8
Hminor <109326297+KHminor@users.noreply.github.com>
2024-07-30 14:27:18 +0900

    [Bronze V] Title: Hello World, Time: 72 ms, Memory: 11204 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,7 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
     }
 }
