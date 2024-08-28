2f664bdef828d1b2fe3a927014277a45f7e4a725
Hminor <109326297+KHminor@users.noreply.github.com>
2024-08-28 15:30:13 +0900

    [D1] Title: 간단한 N 의 약수, Time: 193 ms, Memory: 26,440 KB -BaekjoonHub

---
+++ SWEA/D1/1933. 간단한 N 의 약수/README.md
@@ -0,0 +1,15 @@
+# [D1] 간단한 N 의 약수 - 1933 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PhcWaAKIDFAUq) 
+
+### 성능 요약
+
+메모리: 26,440 KB, 시간: 193 ms, 코드길이: 672 Bytes
+
+### 제출 일자
+
+2024-08-28 15:30
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Solution
+{
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<Integer> li = new HashSet<>();
+        for (int i=1; i<=(int)Math.pow(n,0.5); i++) {
+            if (n%i == 0) {
+                li.add(i);
+                li.add(n/i);
+            }
+        }
+        List<Integer> result = new ArrayList<>(li);
+        Collections.sort(result);
+        System.out.println(result.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+    }
+}
