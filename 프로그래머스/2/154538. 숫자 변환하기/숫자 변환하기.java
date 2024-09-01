4638ac93729d7c4609a02bec14f603befeac008a
Hminor <109326297+KHminor@users.noreply.github.com>
2024-09-01 14:06:56 +0900

    [level 2] Title: 숫자 변환하기, Time: 234.16 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -3,17 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(int x, int y, int n) {
         Queue<int[]> li = new LinkedList<>() {{
-            add(new int[]{y,0});
+            add(new int[]{x,0});
         }};
         Set<Integer> visit = new HashSet<>() {{
-            add(y);
+            add(x);
         }};
         while (!li.isEmpty()) {
             int[] now = li.poll();
-            if (now[0] == x) return now[1]; 
-            if (now[0]-n>=x && !visit.contains(now[0]-n)) li.add(new int[]{now[0]-n,now[1]+1});
-            if (now[0]%2==0 && now[0]/2>=x && !visit.contains(now[0]/2)) li.add(new int[]{now[0]/2,now[1]+1});
-            if (now[0]%3==0 && now[0]/3>=x && !visit.contains(now[0]/3)) li.add(new int[]{now[0]/3,now[1]+1});
+            if (now[0] == y) return now[1];
+            for (int num: new int[]{now[0]+n,now[0]*2,now[0]*3}) {
+                    if (num<=y && !visit.contains(num)) { 
+                        visit.add(num);
+                        li.add(new int[]{num,now[1]+1});
+                    }
+            }
         }
         return -1;
     }
