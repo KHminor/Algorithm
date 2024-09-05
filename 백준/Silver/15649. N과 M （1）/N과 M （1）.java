2e9cde8f021676101f8b14074b3d9ead9652dc14
Hminor <109326297+KHminor@users.noreply.github.com>
2024-09-05 17:02:39 +0900

    [Silver III] Title: N과 M (1), Time: 304 ms, Memory: 30672 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,30 +1,37 @@
 import java.io.*;
 import java.util.*;
 import java.util.stream.*;
+
 public class Main {
     static Deque<Integer> result;
     static boolean[] visit;
-    public static void main(String[] args) throws IOException{
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder(); 
         int[] NM = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int N = NM[0];
         int M = NM[1];
         result = new ArrayDeque<>();
-        visit = new boolean[N+1];
-        int[] num_li = IntStream.rangeClosed(1,N).toArray();
-        back(M,num_li);
-
+        visit = new boolean[N + 1];
+        int[] num_li = IntStream.rangeClosed(1, N).toArray();
+        back(M, num_li);
+        System.out.println(sb.toString());
     }
+
     public static void back(int M, int[] num_li) {
         if (result.size() == M) {
-            System.out.println(result.stream().map(String::valueOf).collect(Collectors.joining(" ")));
-            return ;
+            for (int num: result) sb.append(num).append(" ");
+            sb.append("\n");
+            return;
         }
-        for (int num: num_li) {
+
+        for (int num : num_li) {
             if (!visit[num]) {
                 result.add(num);
                 visit[num] = true;
-                back(M,num_li);
+                back(M, num_li);
                 result.pollLast();
                 visit[num] = false;
             }
