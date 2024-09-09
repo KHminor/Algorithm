4ed56ca7150b775043aa69e76eba20e4f892493c
Hminor <109326297+KHminor@users.noreply.github.com>
2024-09-09 16:50:16 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 464 ms, Memory: 36056 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -13,16 +13,14 @@ public class Main {
         int[][] mtx = new int[N+1][M+1];
         for (int t=1; t<=N; t++) {
             st = new StringTokenizer(br.readLine());
-            for (int c=1; c<=M; c++) mtx[t][c] = Integer.parseInt(st.nextToken());
+            for (int c=1; c<=M; c++) mtx[t][c] = mtx[t][c-1]+Integer.parseInt(st.nextToken());
         }
 
         int K = Integer.parseInt(br.readLine());
         for (int t=0; t<K; t++) {
             int[] li = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             int hap = 0;
-            for (int y=li[0]; y<=li[2]; y++) {
-                for (int x=li[1]; x<=li[3]; x++) hap += mtx[y][x];
-            }
+            for (int y=li[0]; y<=li[2]; y++) hap += mtx[y][li[3]]-mtx[y][li[1]-1];
             bw.write(hap+"\n");
         }
         bw.flush();
