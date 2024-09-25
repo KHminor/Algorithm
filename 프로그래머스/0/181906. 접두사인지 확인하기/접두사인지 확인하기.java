4bc20219d815f896c16f5be111b10f2bfbd52c23
Hminor <109326297+KHminor@users.noreply.github.com>
2024-09-26 00:14:46 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.10 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        return my_string.indexOf(is_prefix)==0 ? 1:0;
+        return my_string.startsWith(is_prefix)?1:0;
     }
 }
