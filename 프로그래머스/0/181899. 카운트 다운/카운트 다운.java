c4728844d8bb8cf92eaebda104151d2b0347e1c7
Hminor <109326297+KHminor@users.noreply.github.com>
2024-09-25 23:49:49 +0900

    [level 0] Title: 카운트 다운, Time: 7.98 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,11 +1,6 @@
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num-end_num+1];
-        int cnt = 0;
-        for (int i=start_num; i>=end_num; i--) {
-            answer[cnt] = i;
-            cnt++;
-        }
-        return answer;
+        return IntStream.rangeClosed(-start_num,-end_num).map(i->-i).toArray();
     }
 }
