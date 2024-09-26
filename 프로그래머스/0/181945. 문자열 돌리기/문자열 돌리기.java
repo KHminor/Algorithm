060eea1cb6c2a05962d10d4599dbe99a9d932510
Hminor <109326297+KHminor@users.noreply.github.com>
2024-09-26 23:02:26 +0900

    [level 0] Title: 문자열 돌리기, Time: 156.83 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] li = sc.next().toCharArray();
+        for (char i: li) System.out.println(i);
+    }
+}
