36859888ed9a7cd06259e5e00976928458dd74d6
Hminor <109326297+KHminor@users.noreply.github.com>
2024-10-01 17:17:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,21 +1,30 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int mx_health = health;
-        int a_idx = 0;
-        int b_cnt = 0;
-        for (int i=1; i<=attacks[attacks.length-1][0]; i++) {
-            if (attacks[a_idx][0]==i) {
-                // 체력 회복부터
-                health += bandage[1]*b_cnt+b_cnt/bandage[0]*bandage[2];
-                if (health>mx_health) health=mx_health;
+        int now_health = health;
+        int now = 1;
+        
+//         for (int i=1; i<=attacks[attacks.length-1][0]; i++) {
+//             if (attacks[a_idx][0]==i) {
+//                 // 체력 회복부터
+//                 health += bandage[1]*b_cnt+b_cnt/bandage[0]*bandage[2];
+//                 if (health>mx_health) health=mx_health;
                 
-                health-=attacks[a_idx][1];
-                a_idx++;
-                b_cnt = 0;
-                if (health<=0) return -1;
-            } else b_cnt++;
+//                 health-=attacks[a_idx][1];
+//                 a_idx++;
+//                 b_cnt = 0;
+//                 if (health<=0) return -1;
+//             } else b_cnt++;
+//         }
+        for (int[] atk: attacks) {
+            int diff = atk[0]-now;
+            now_health += diff*bandage[1]+diff/bandage[0]*bandage[2];
+            if (now_health>health) now_health = health;
+            now_health -= atk[1];
+            if (now_health<=0) return -1;
+            now = atk[0]+1;
         }
         
-        return health;
+        
+        return now_health;
     }
 }
