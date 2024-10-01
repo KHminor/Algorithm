5687651c07b93fd2daaac152992121bbe5748dc2
Hminor <109326297+KHminor@users.noreply.github.com>
2024-10-01 14:41:13 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 22.05 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,10 +1,12 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        for (int[] li: queries) {
-            int num = arr[li[0]];
-            arr[li[0]] = arr[li[1]];
-            arr[li[1]] = num;
-        }
-        return arr;
+        List<Integer> li = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        for (int[] q: queries) Collections.swap(li, q[0], q[1]);
+        return li.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
