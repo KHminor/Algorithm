c0085ce7571360a311401cbabe4861807274cd57
Hminor <109326297+KHminor@users.noreply.github.com>
2024-10-01 16:07:49 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.52 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] q: queries) {
+            for (int i=q[0]; i<=q[1]; i++) {
+                if (i%q[2]==0) arr[i]+=1;
+            }
+        }
+        return arr;
+    }
+}
