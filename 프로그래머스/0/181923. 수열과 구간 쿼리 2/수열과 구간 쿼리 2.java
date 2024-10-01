01524a0dddd725ed0a9ddc822f02066bb6d9e23a
Hminor <109326297+KHminor@users.noreply.github.com>
2024-10-01 15:35:28 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 15.16 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,15 +1,25 @@
+import java.util.stream.IntStream;
 class Solution {
+    // public int[] solution(int[] arr, int[][] queries) {
+    //     int[] answer = new int[queries.length];
+    //     int idx = 0;
+    //     for (int[] li: queries) {
+    //         int num = 1000001;
+    //         for (int i=li[0]; i<=li[1]; i++) {
+    //             if (arr[i]>li[2] && num>arr[i]) num = arr[i];
+    //         }
+    //         answer[idx] = num==1000001?-1:num;
+    //         idx++;
+    //     }
+    //     return answer;
+    // }
+    
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-        int idx = 0;
-        for (int[] li: queries) {
-            int num = 1000001;
-            for (int i=li[0]; i<=li[1]; i++) {
-                if (arr[i]>li[2] && num>arr[i]) num = arr[i];
-            }
-            answer[idx] = num==1000001?-1:num;
-            idx++;
-        }
-        return answer;
+        return IntStream.range(0,queries.length)
+            .map(i->IntStream.rangeClosed(queries[i][0],queries[i][1])
+                .map(j-> arr[j])
+                .filter(j->j>queries[i][2])
+                .min().orElse(-1)
+                ).toArray();
     }
 }
