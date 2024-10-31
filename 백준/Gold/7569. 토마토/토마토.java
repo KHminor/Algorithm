dd7449f294c44500ff95ee4832cc0f9bc13f973e
Hminor <109326297+KHminor@users.noreply.github.com>
2024-10-31 16:27:55 +0900

    [Gold V] Title: 토마토, Time: 1008 ms, Memory: 175604 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -9,33 +9,13 @@ public class Main {
         int[] nk = {0,1,0,-1};
         Deque<List<Integer>> dq = new ArrayDeque<>();
         StringTokenizer st;
+        int cnt = 0;
         for (int i=0; i<MNH[2]; i++) {
             for (int j=0; j<MNH[1]; j++) {
                 st = new StringTokenizer(br.readLine());
                 for (int k=0; k<MNH[0]; k++) {
                     mtx[i][j][k] = Integer.parseInt(st.nextToken());
-                }
-            }
-        }
-        int cnt = 0;
-
-        // 안 익은 토마토 주위에 있는 익은 토마토 위치 추가, 안 익은 토마토 개수 체크
-        for (int i=0; i<MNH[2]; i++) {
-            for (int j=0; j<MNH[1]; j++) {
-                for (int k=0; k<MNH[0]; k++) {
-                    if (mtx[i][j][k]==1) {
-//                        boolean state = false;
-//                        for (int x=0; x<4; x++) {
-//                            if ((0<=j+nj[x] && j+nj[x]<MNH[1]) && (0<=k+nk[x] && k+nk[x]<MNH[0]) && mtx[i][j+nj[x]][k+nk[x]]==0) {
-//                                state = true;
-//                                break;
-//                            }
-//                        }
-//                        if (0<=i-1 && mtx[i-1][j][k]==1) state = true;
-//                        else if (i+1<MNH[2] && mtx[i+1][j][k]==1) state = true;
-//                        if (state)
-                        dq.add(new ArrayList<>(Arrays.asList(i,j,k,0)));
-                    }
+                    if (mtx[i][j][k]==1) dq.add(new ArrayList<>(Arrays.asList(i,j,k,0)));
                     else if (mtx[i][j][k]==0) cnt++;
                 }
             }
@@ -46,7 +26,6 @@ public class Main {
         while (true) {
 
             if (dq.isEmpty()) break;
-            // 이제 익은 토마토 주위에 있는, 익지 않은 토마토 익히기
             while (!dq.isEmpty()) {
                 List<Integer> li = dq.pollFirst();
                 result = Math.max(result, li.get(3));
@@ -68,8 +47,6 @@ public class Main {
                     cnt--;
                 }
             }
-//            result ++;
-//            cnt = 0;
         }
 
         System.out.println(cnt!=0?-1:result);
