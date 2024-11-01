0dada3f661b8af488bd1265952f38662ec8ea8b0
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-01 15:21:43 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1152 ms, Memory: 285124 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> citys = new ArrayList<>();
+        for (int i=0; i<=N; i++) citys.add(new ArrayList<>());
+        long[] cost = new long[N+1];
+        Arrays.fill(cost,-1);
+
+        for (int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            citys.get(Integer.parseInt(st.nextToken())).add(Integer.parseInt(st.nextToken()));
+        }
+
+        Deque<Integer> dq = new ArrayDeque<>(Arrays.asList(X));
+        cost[X] = 0;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (!dq.isEmpty()) {
+            int now = dq.pollFirst();
+            if (cost[now]==K) result.add(now);
+            else {
+                for (Integer val: citys.get(now)) {
+                    if (cost[val]==-1 && cost[now]+1<=K) {
+                        cost[val] = cost[now]+1;
+                        dq.add(val);
+                    }
+                }
+            }
+        }
+        if (result.isEmpty()) System.out.println(-1);
+        else {
+            Collections.sort(result);
+            for (int num: result) bw.write(num+"\n");
+            bw.flush();
+        }
+    }
+}
