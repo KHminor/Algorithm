a31297d2d9111824c318a33c0638a0a849c4b98a
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-02 09:46:01 +0900

    [level 1] Title: K번째수, Time: 1.34 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
+ 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for (int i=0; i<commands.length; i++) {
             int[] li = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
             Arrays.sort(li);
-            answer[i] = li[commands[i][2]-1];
+            answer[i] = li[commands[i][2]-1]; 
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 1.13 ms
+메모리: 74.3 MB, 시간: 1.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 02일 09:01:23
+2024년 11월 02일 09:45:58
 
 ### 문제 설명
 
