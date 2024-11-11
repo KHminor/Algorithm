3a0eebd136b52e81cef0387576a3443bedd672a2
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-11 19:37:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 612 ms, Memory: 60160 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -7,20 +7,17 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        Map<String, String> dic = new HashMap<>();
-        for (int i=0; i<N; i++) {
+        Set<String> s_li = new HashSet<>();
+        while (N>0) {
             st = new StringTokenizer(br.readLine());
-            String k = st.nextToken();
-            String v = st.nextToken();
-            dic.put(k,v);
+            String nm = st.nextToken();
+            String cmd = st.nextToken();
+            if (cmd.equals("enter")) s_li.add(nm);
+            else s_li.remove(nm);
+            N--;
         }
-        List<String> result = new ArrayList<>();
-        List<String> dic_li = new ArrayList<>(dic.keySet());
-        Collections.sort(dic_li);
-        for (int i=0; i<dic_li.size(); i++) {
-            if (dic.get(dic_li.get(i)).equals("enter")) result.add(dic_li.get(i));
-        }
-        Collections.reverse(result);
+        List<String> result = new ArrayList<>(s_li);
+        Collections.sort(result, Collections.reverseOrder());
         for (String name: result) bw.write(name+"\n");
         bw.flush();
     }
