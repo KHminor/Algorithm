c55202aae42faa7b5f5fb7c61dd6c4399373a3b3
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-12 15:06:21 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 872 ms, Memory: 150936 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        String[] pokemon = new String[N];
+        Map<String, Integer> dic = new HashMap<>(N);
+        for (int i=0; i<N; i++) {
+            String poke = br.readLine();
+            pokemon[i] = poke;
+            dic.put(poke,i+1);
+        }
+        while (M>0) {
+            String inp = br.readLine();
+            if (isNum(inp)) bw.write(pokemon[Integer.parseInt(inp)-1]+"\n");
+            else bw.write(dic.get(inp)+"\n");
+            M--;
+        }
+        bw.flush();
+    }
+
+    public static boolean isNum(String str) {
+        return str.matches("-?\\d+(\\.\\d+)?");
+    }
+}
