c0ae3a9e4b3affe7a6a58b1e6c8fe0b7f5038417
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-18 14:34:10 +0900

    [Bronze IV] Title: 녹색거탑, Time: 92 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)Math.pow(2,sc.nextInt()));
+    }
+}
