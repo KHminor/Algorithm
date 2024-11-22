edd5d5b6125e9bd8173fe77465d63bbee9a17a3a
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-22 15:15:40 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1048 ms, Memory: 138772 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,16 +9,13 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        long[][] mtx = new long[N][N];
-        long[][] check = new long[N+1][N+1];
+        long[][] mtx = new long[N][N+1];
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; j++) {
-                mtx[i][j]=Integer.parseInt(st.nextToken());
-                check[i+1][j+1]=check[i+1][j]+mtx[i][j];
+            for (int j=1; j<=N; j++) {
+                mtx[i][j]=Integer.parseInt(st.nextToken())+mtx[i][j-1];
             }
         }
-
         while (M>0) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
@@ -26,7 +23,7 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
             long hap = 0L;
-            for (int i=x1; i<=x2; i++) hap+=check[i][y2]-check[i][y1-1];
+            for (int i=x1-1; i<x2; i++) hap+=mtx[i][y2]-mtx[i][y1-1];
             bw.write(hap+"\n");
             M--;
         }
