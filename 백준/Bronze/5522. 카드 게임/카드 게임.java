bab0d3af11f95fb8978faecdd4692d99cd4c5220
Hminor <109326297+KHminor@users.noreply.github.com>
2024-11-30 22:08:33 +0900

    [Bronze V] Title: 카드 게임, Time: 100 ms, Memory: 12844 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        for (int i=0; i<5; i++) result+=sc.nextInt();
+        System.out.println(result);
+    }
+}
