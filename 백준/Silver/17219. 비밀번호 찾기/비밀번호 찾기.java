17dabafee5f171a7e05d3e14cc5fdf2c9593221d
Hminor <109326297+KHminor@users.noreply.github.com>
2024-12-05 14:47:51 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 512 ms, Memory: 72304 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -2,22 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static private Map<String, String> dic = new HashMap<>();
+    static private StringTokenizer st;
+    static private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static private BufferedWriter bw;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        Map<String,String> dic = new HashMap<>();
-        while (N>0) {
+
+        addMapData(N);
+        findPwd(M);
+        bw.flush();
+    }
+
+    private static void addMapData(int N) throws IOException {
+        while (N > 0) {
             st = new StringTokenizer(br.readLine());
-            dic.put(st.nextToken(),st.nextToken());
+            dic.put(st.nextToken(), st.nextToken());
             N--;
         }
-        while (M>0) {
-            bw.write(dic.get(br.readLine())+'\n');
+    }
+
+    private static void findPwd(int M) throws IOException {
+        while (M > 0) {
+            bw.write(dic.get(br.readLine()) + '\n');
             M--;
         }
-        bw.flush();
     }
 }
