72851b7a4d4f2d6941e4f38dd5c6ee88023dab68
Hminor <109326297+KHminor@users.noreply.github.com>
2024-12-10 16:34:56 +0900

    [Silver I] Title: IOIOI, Time: 156 ms, Memory: 20508 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,20 +5,24 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         String s = br.readLine();
-        System.out.println(findResult(M,N*2+1,makeString(N),s));
+        System.out.println(findResult(M,N*2+1,s));
     }
-    private static String makeString(int N) {
-        StringBuilder sb = new StringBuilder();
-        for (int i=0; i<N; i++) sb.append("IO");
-        sb.append("I");
-        return sb.toString();
+    private static int findResult(int M, int ms_ln, String s) {
+        int cnt = 0;
+        int ln = 0;
+        char[] check = {'I','O'};
+        for (int i=0; i<M; i++) {
+            if (check[ln%2]==s.charAt(i)) ln++;
+            else {
+                cnt+=cal(ms_ln, ln);
+                ln=check[0]==s.charAt(i)?1:0;
+            }
+        }
+        if (ln!=0) cnt+=cal(ms_ln, ln);
+        return cnt;
     }
 
-    private static int findResult(int M, int ms_ln, String ms, String s) {
-        int cnt = 0;
-        for (int i=0; i<=M-ms_ln; i++) {
-            if (s.substring(i,i+ms_ln).equals(ms)) cnt++;
-        }
-        return cnt;
+    private static int cal(int ms_ln, int ln) {
+        return ln>=ms_ln?(ln-ms_ln)/2+1:0;
     }
 }
