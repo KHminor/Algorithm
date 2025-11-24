e1b1b7e3aa3139b40e7ebcd493c5ebc6b6273397
Hminor <109326297+KHminor@users.noreply.github.com>
2025-11-24 16:30:46 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 396 ms, Memory: 55244 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,21 +1,25 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb;
-        int t = Integer.parseInt(br.readLine());
-        for (int i=0; i<t; i++) {
-            String[] li = br.readLine().split(" ");
-            for (int x=0; x<li.length; x++) {
-                sb = new StringBuilder("");
-                for (int j=li[x].length()-1; j>=0; j--) sb.append(li[x].charAt(j));
-                li[x] = sb.toString();
+        int T = Integer.parseInt(br.readLine());
+        while (T!=0) {
+            String[] arr = br.readLine().split(" ");
+            sb = new StringBuilder();
+            for (int i=0; i<arr.length; i++) {
+                for (int j=arr[i].length()-1; j>=0; j--) {
+                    sb.append(arr[i].charAt(j));
+                }
+                sb.append(" ");
             }
-            bw.write(String.join(" ", li) +"\n");
+            bw.write(sb+"\n");
+            T--;
         }
         bw.flush();
+        bw.close();
     }
 }
