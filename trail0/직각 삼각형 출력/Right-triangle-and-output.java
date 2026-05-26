79ce521a210ae111e6bed0e99dc33c21ab71c009
Hminor <109326297+KHminor@users.noreply.github.com>
2026-05-26 21:47:29 +0900

    [Passed] 직각 삼각형 출력 (136ms, 10MB)

--- trail0/직각 삼각형 출력/Right-triangle-and-output.java
+++ trail0/직각 삼각형 출력/Right-triangle-and-output.java
@@ -6,8 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i=0; i<n; i++) {
-            System.out.print("*");
-            for (int j=0; j<2*i; j++) {
+            for (int j=0; j<2*i+1; j++) {
                 System.out.print("*");
             }
             System.out.println();
