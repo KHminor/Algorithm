bc2e7557e604bf502a2cf04ba0e70ebc417e1e7c
Hminor <109326297+KHminor@users.noreply.github.com>
2026-05-27 22:14:24 +0900

    [Passed] 숫자 피라미드 (157ms, 11MB)

---
+++ trail0/숫자 피라미드/number-pyramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            for (int j=0; j<i; j++) {
+                System.out.printf("%d ",i);
+            }
+            System.out.println();
+        }
+    }
+}
