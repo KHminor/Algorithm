a49fb086c9b8416b820941410d618ae51348a39a
Hminor <109326297+KHminor@users.noreply.github.com>
2026-05-29 21:39:44 +0900

    [Passed] 변수 값 교체하기 3 (95ms, 9MB)

---
+++ trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 3;
+        int b = 5;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
