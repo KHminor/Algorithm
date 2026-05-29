d346a31b29789c7c5cc107a549ed0758a00c46f2
Hminor <109326297+KHminor@users.noreply.github.com>
2026-05-29 21:06:53 +0900

    [Passed] 좌우로 반복하며 출력 (134ms, 10MB)

---
+++ trail0/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean state = true;
+        for (int i=0; i<n; i++) {
+            if (!state) {
+                for (int j=0; j<n; j++) {
+                    System.out.print(n-j);
+                }
+            } else {
+                for (int j=n-1; j>=0; j--) {
+                    System.out.print(n-j);
+                }
+            }
+            System.out.println();
+            state = !state;
+        }
+    }
+}
